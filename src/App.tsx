82c00c046e2283669463a8931e603987a4291654
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-13 19:26:55 +0800

    The input of city

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { KeyboardEvent, useCallback, useEffect, useState } from 'react'
 
 import './App.scss'
 
@@ -6,7 +6,7 @@ function App() {
   const [city, setCity] = useState('Paris')
   const [weatherInfo, setWeatherInfo] = useState(null)
   const [error, setError] = useState<unknown>(null)
-  const [videoSource, setVideoSource] = useState('')
+  const [videoSource, setVideoSource] = useState<string>('./videos/clear sky.mp4')
 
   function setVideoBackground(weatherCondition: string) {
     weatherCondition = weatherCondition.toLowerCase()
@@ -40,10 +40,10 @@ function App() {
     }
   }
 
-  const getWeather = useCallback(async () => {
+  const getWeather = useCallback(async (c = 'Paris') => {
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        `https://api.openweathermap.org/data/2.5/weather?q=${c}&units=metric&appid=${
           import.meta.env.VITE_API_KEY
         }`
       )
@@ -54,12 +54,23 @@ function App() {
     } catch (err: unknown) {
       setError(() => err)
     }
-  }, [city])
+  }, [])
 
+  function handleKeyUp(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === 'Enter') {
+      const c = (e.target as HTMLInputElement).value
+      getWeather(c)
+      setCity(() => c)
+    }
+  }
   useEffect(() => {
     getWeather()
   }, [getWeather])
 
+  useEffect(() => {
+    console.log(weatherInfo)
+  }, [weatherInfo])
+  
   return (
     <div className='page'>
       <video
@@ -70,6 +81,31 @@ function App() {
         playsInline
         className='video-background'
       ></video>
+      <main className='main'>
+        <div className='container'>
+          <div className='laptop'>
+            <div className='sections'>
+              <section
+                className={[
+                  'section',
+                  'section-left',
+                  error ? 'section-error' : '',
+                ].join(' ')}
+              >
+                <div className='info'>
+                  <div className='city-inner'>
+                    <input
+                      type='text'
+                      className='search'
+                      onKeyUp={handleKeyUp}
+                    />
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
