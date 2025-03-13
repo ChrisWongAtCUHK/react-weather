3e0fd11b1ccfbd83ebb046abfc5ed6f04f2e9087
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-13 20:26:54 +0800

    Highlights wind

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,9 +1,31 @@
 import './Highlights.scss'
 
-function Highlights() {
+function Highlights({ weatherInfo }) {
   return (
     <div className='section highlights'>
       <div className='title'>Today's Highlights</div>
+      <div className='highlights-wrapper'>
+        <div className='highlight'>
+          <div className='card'>
+            <div className='card-title'>Wind</div>
+            <div className='card-pic card-pic--wind'></div>
+            <div className='card-info'>
+              <div className='card-justify'>
+                <div className='info-main'>
+                  <div className='info-main-num'>
+                    {weatherInfo?.wind?.speed}
+                  </div>
+                  <div className='info-main-text'>m/s</div>
+                </div>
+                <div className='info-main'>
+                  <div className='info-main-num'>{weatherInfo?.wind?.deg}</div>
+                  <div className='info-main-text'>deg</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
