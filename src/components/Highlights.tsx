54d53cde6385988a4b3b8ef010f4e07d2656ad3a
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-14 14:13:17 +0800

    Wind gusts

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -24,6 +24,30 @@ function Highlights({ weatherInfo }) {
               </div>
             </div>
           </div>
+          <div className='card-small'>
+            <div className='card-small-title'>Wind gusts</div>
+            <div className='card-small-info'>
+              <div v-if='weatherInfo?.wind?.gust' className='card-small-data'>
+                <div className='info-main-num'>
+                  {Math.round(weatherInfo?.wind?.gust)}
+                </div>
+                <div className='info-main-text'>m/s</div>
+              </div>
+              <div className='card-small-hint'>
+                <div className='card-small-pic card-small-pic--wind'></div>
+                <div className='card-small-text text-egorova'>
+                  Learn&nbsp;
+                  <a
+                    href='https://www.windy.com/articles/weather-phenomena-what-s-the-difference-between-sustained-winds-and-wind-gusts-10390?satellite,7.787,115.115,5'
+                    target='_blank'
+                  >
+                    more
+                  </a>
+                  &nbsp;about gusts
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
